57759babefbd0f74cbe7cc071e45cac2dc03e81d
haouari-mohamed <haouari-mohamed>
2025-01-31 21:33:01 +0100

    create the project with the dependencies i will use

---
+++ src/main/java/com/example/dialy/bias/tool/DialyBiasToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.dialy.bias.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialyBiasToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialyBiasToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dialy/bias/tool/DialyBiasToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dialy.bias.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialyBiasToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
